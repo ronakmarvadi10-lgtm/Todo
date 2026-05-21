cae6b97df79d85241f506428f262a417e196dcf5
ronakmarvadi10-lgtm <ronakmarvadi10@gmail.com>
2026-05-21 14:07:08 +0530

    Add files via upload

---
+++ Todoo.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+
+export default function Todoo() {
+    const [state,setState]=useState('')
+    const [state1,setState1]=useState([])
+
+    function AddText(e){
+       setState( e.target.value)
+
+    }
+
+    function handleSubmit(e){
+       e.preventDefault()
+       setState1([...state1,state])
+    }
+
+    function deleteEn(id){
+           console.log(id)
+        var ans=state1.filter((el,i)=>{
+            return i!== id
+        })
+        setState1(ans)
+    }
+
+  return (
+    <div>
+      <h1>Todo</h1>
+     <form action="" onSubmit={handleSubmit}>
+         <input type="text" placeholder='enter name' value ={state} onChange={AddText}/>
+      <input type='submit'/>
+     </form>
+
+     {
+        state1.map((el,i)=>{
+            return <>
+                   <li key={i}>{el}</li>
+                   <button onClick={()=>deleteEn(i)}>Click</button>
+            </>
+        })
+     }
+    </div>
+  )
+}
